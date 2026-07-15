b5e1c5b1df0eafe40aecb44b30ebcfd1e38a9ee7
Chintal <chintal@cobraclicks.com>
2026-07-15 12:43:14 +0530

    Added Images

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -56,10 +56,7 @@ export interface FileRoutesById {
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | '/'
-    | '/services/$slug'
-    | '/services/$slug/$project'
-    | '/services/$slug/'
+    '/' | '/services/$slug' | '/services/$slug/$project' | '/services/$slug/'
   fileRoutesByTo: FileRoutesByTo
   to: '/' | '/services/$slug/$project' | '/services/$slug'
   id:
@@ -129,3 +126,13 @@ const rootRouteChildren: RootRouteChildren = {
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
+
+import type { getRouter } from './router.tsx'
+import type { startInstance } from './start.ts'
+declare module '@tanstack/react-start' {
+  interface Register {
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
+    config: Awaited<ReturnType<typeof startInstance.getOptions>>
+  }
+}
